c2ed28851c2a059dff45dd7eb07f03f90c5774d8
yannisobert <90550556+yannisobert@users.noreply.github.com>
2022-05-25 21:04:58 +0200

    Setup Project

---
+++ src/Command/ProductCommand.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Command;
+
+use Symfony\Component\Console\Command\Command;
+
+class ProductCommand extends Command
+{
+    public function __construct(
+
+    )
+    {
+        parent::__construct();
+    }
+
+}
