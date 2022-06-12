773e9680a2df2f7bdd7f86f5ed096e3ee9d43ee5
yannisobert <90550556+yannisobert@users.noreply.github.com>
2022-06-12 22:38:15 +0200

    Add improvements

--- src/Command/ProductCommand.php
+++ src/Command/ProductCommand.php
@@ -2,24 +2,21 @@
 
 namespace App\Command;
 
+use App\Service\Read;
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Helper\Table;
-use Symfony\Component\Console\Helper\TableSeparator;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 
-
 class ProductCommand extends Command
 {
     protected static $defaultName = 'app:csv-information';
 
-    function __construct(
-        $projectDir
-    )
+    function __construct($projectDir, Read $read)
     {
         $this->projectDir = $projectDir;
+        $this->read = $read;
 
         parent::__construct();
     }
@@ -27,7 +24,8 @@ class ProductCommand extends Command
     function configure()
     {
         $this->setDescription('Description.')
-            ->addArgument('file', InputArgument::REQUIRED, 'csv name.')
+            ->addArgument('path', InputArgument::REQUIRED, 'csv path.')
+            ->addArgument('name', InputArgument::REQUIRED, 'csv name.')
             ->addArgument('json', InputArgument::OPTIONAL, 'json.');
     }
 
@@ -35,81 +33,28 @@ class ProductCommand extends Command
     {
         $io = new SymfonyStyle($input, $output);
 
-        $nameFile = $input->getArgument('file');
+        // Argument retrieval
+        $pathCsv = $input->getArgument('path');
+        $nameCsv = $input->getArgument('name');
         $json = $input->getArgument('json');
 
-        if ($nameFile !== 'products') {
-            $io->warning('The first argument doesn\'t exist, please retry.');
-
-            return 0;
-        }
-
+        // Verification if we have a third argument
         if ($json)
         {
+            // Verification if the third argument is good
             if ($json !== 'json') {
-                $io->warning('The second argument doesn\'t exist, please retry.');
+                $io->warning('The third argument doesn\'t exist, please retry.');
 
-                return 0;
+                return 1;
             }
         }
 
-        $productCsv = $this->projectDir . '/public/csv/' . $nameFile . '.csv';
+        // Path to the CSV
+        $productCsv = $this->projectDir . $pathCsv . $nameCsv . '.csv';
 
-        $csvOpen = fopen($productCsv, 'rb');
+        //formatter
+        $this->read->csvToArray($productCsv, $input, $output, $io, $json);
 
-        $true = true;
-
-        $table = new Table($output);
-
-        $array = [];
-
-        while (($document = fgetcsv($csvOpen, 1000, ';')) !== false) {
-            if ($true) {
-
-                $table->setHeaders([$document[0], 'Status', $document[3], $document[5], 'Created At', 'Slug']);
-                $true = false;
-
-                $titleArray = [$document];
-
-                continue;
-            } else {
-                $table->addRow(new TableSeparator());
-            }
-
-            $date = new \DateTime($document[6]);
-            $slug = preg_replace('/[^A-Za-z0-9\-]/', '-', str_replace(' ', '_',
-                strtolower($document[1])));
-            $price = str_replace('.', ',', $document[3]);
-            $currency = strip_tags($document[4]);
-
-            $description = str_replace('<br/>', "\n",str_replace('\r', "\n",
-                $document[5]));
-
-            $table->addRow([$document[0], ($document[2] ? 'Enable' : 'Disable'), $price . $currency, $description,
-                $date->format('l, j-M-Y H:i:s T'), $slug]);
-
-            $content = [
-                $titleArray[0][0] =>$document[0],
-                $titleArray[0][3] => $price . $currency,
-                'Status' => $document[2] ? 'Enable' : 'Disable',
-                $titleArray[0][5] => $document[5],
-                'Created At' => $date->format('l, j-M-Y H:i:s T'),
-                'Slug' => $slug
-            ];
-
-            array_push($array, $content);
-        }
-
-        $json = json_encode($array, JSON_PRETTY_PRINT);
-
-        if ($input->getArgument('json') === 'json') {
-            echo $json;
-        } else {
-            $table->render();
-        }
-
-        $io->success('DONE.');
-
-        return 1;
+        return 0;
     }
 }
